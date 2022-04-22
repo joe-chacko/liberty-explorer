b13d9050457ec1382024fff3444312daa73b615e
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2022-04-22 22:19:02 +0100

    Create trivial main class

--- src/main/java/io/openliberty/explorer/Main.java
+++ src/main/java/io/openliberty/explorer/Main.java
@@ -1,2 +1,20 @@
-package io.openliberty.explorer;public class Main {
+/*
+ =============================================================================
+ Copyright (c) 2022 IBM Corporation and others.
+ All rights reserved. This program and the accompanying materials
+ are made available under the terms of the Eclipse Public License v2.0
+ which accompanies this distribution, and is available at
+ http://www.eclipse.org/legal/epl-v20.html
+
+ Contributors:
+      IBM Corporation - initial API and implementation
+  =============================================================================
+ */
+
+package io.openliberty.explorer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
