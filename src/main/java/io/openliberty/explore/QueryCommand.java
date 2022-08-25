a75a6f66d8d1e1f96f0f274b6c70d94f02315388
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2022-08-25 15:35:59 +0100

    Add scope display option

--- src/main/java/io/openliberty/explore/QueryCommand.java
+++ src/main/java/io/openliberty/explore/QueryCommand.java
@@ -12,7 +12,9 @@
  */
 package io.openliberty.explore;
 
+import io.openliberty.inspect.Bundle;
 import io.openliberty.inspect.Element;
+import io.openliberty.inspect.feature.Feature;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.ParentCommand;
@@ -59,7 +61,28 @@ abstract class QueryCommand implements Callable<Integer> {
     @Option(names = {"--display", "-d"}, description = "Control how elements are displayed: ${COMPLETION-CANDIDATES}")
     private DisplayOption display = DisplayOption.normal;
 
+    @Option(names = {"-s", "--scope"}, description = "Display additional scope information:" +
+            "\n\t +++ - public features" +
+            "\n\t === - protected features" +
+            "\n\t --- - private features" +
+            "\n\t -A- - auto-features" +
+            "\n\t -b- - bundles" +
+            "\n\t ??? - unknown")
+    private boolean scope = true;
+
+    private String prefix(Element e) {
+        if (!scope) return "";
+        if (e instanceof Bundle) return "-b- ";
+        if (e.isAutoFeature()) return "-a- ";
+        switch(e.visibility()) {
+            case PUBLIC: return "+++ ";
+            case PROTECTED: return "=== ";
+            case PRIVATE: return "--- ";
+            default: return "??? ";
+        }
+    }
+
     String displayName(Element e) {
-        return display.getName(e);
+        return prefix(e) + display.getName(e);
     }
 }
